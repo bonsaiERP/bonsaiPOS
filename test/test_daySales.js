ffce33adc389a7a775b1c0d6d6977b23f4858af1
FRANZ BELTRAN <franzbeltran79@gmail.com>
2015-10-03 17:59:35 -0700

    arreglando una prueba unitaria

--- test/test_daySales.js
+++ test/test_daySales.js
@@ -8,7 +8,7 @@ describe('GET#time', function() {
   it('Funcion thehighttime deberia obtener true si la hora1 es mayor a hora2', function() {
     assert.equal(daySales.thehighttime(new Date(2015,9,21,01,02,05), new Date(2015,9,21,01,02,04)), true);
   });
- it('Funcion getactualtime deberia obtener la hora en horas:min-seg', function() {
+ it('Funcion getactualtime deberia obtener la hora en horas:min:seg', function() {
     assert.equal(daySales.getactualtime(new Date(2015,9,21,01,02,05)), '1:2:5');
   });
 });
