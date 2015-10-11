5ea8accab283aac825aa66aa4ac8f57f93d51e9f
alexflores <axel_f_g@hotmail.com>
2015-10-11 13:39:50 -0700

    Funcionalidad cerrar caja concluida

--- models/cashier/cashier.js
+++ models/cashier/cashier.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
-
+//intente llamar aca al archivo daySales.js pero no da, esto para no repetir codigo que ya existe
+//en otras palabras quice reusar codigo de este archivo !!!
+//var daySales = '../reports/daySales.js';
+//document.write('<script src="../reports/daySales.js" type="text/javascript"></script>');
 
 function showAlertMessage(tipeMessage)
 {
@@ -10,7 +13,7 @@ function showAlertMessage(tipeMessage)
   }
   else if (tipeMessage=="warningAmount"){
     $("#alertMessage").addClass("alert alert-dismissible alert-danger");
-    $("#alertMessage")[0].innerHTML='<p>La cantidad debe ser numerica y mayor a cero.</p>';
+    $("#alertMessage")[0].innerHTML='<p>Se produjo un error al procesar el cierre de caja.</p>';
   }
   $("#alertMessage").show();
 }
@@ -24,14 +27,83 @@ function showAlertMessage(tipeMessage)
     
     if(money > 0){
        create_cashier(money);
-       showAlertMessage("success");
-       $("#money").val("");
+       location.href = "../../views/cashier/closecashier.html";
     }else {
        showAlertMessage("warningAmount");
     }
   });
 
+  $("#btn_confirm").click(function () {
+      var total_cash = data_cashier(); 
+      if(total_cash > 0){
+        close_cashier(total_cash);
+        location.href = "../../views/cashier/opencashier.html";
+      }else {
+        showAlertMessage("warningAmount");
+      }
+  });
+
+
 })(jQuery);
+
+function convertdatetoformatofso(date)
+{
+  return new Date(date);
+}
+
+function returnlistofsalesofdate(date)
+{
+  var db = new DataBase();
+  var general_list_of_sales = db.getTable("sales",'\\views\\cashier',2);
+  var day = date.getDate();
+  var month = date.getMonth();
+  var year = date.getFullYear();
+  var result = [];
+  for (i = 0; i < general_list_of_sales.length; i++) {
+    var aux = convertdatetoformatofso(general_list_of_sales[i].date);
+    if(aux.getDate() == day && aux.getMonth() == month && aux.getFullYear() == year)
+    {
+      result.push(general_list_of_sales[i]);
+    }
+  }
+  return result;
+}
+
+function gettotalofsales(list_of_sales)
+{
+  var tot = 0;
+  for (var cont = 0; cont < list_of_sales.length; cont++) {
+    tot = tot + list_of_sales[cont].total;
+  }
+  return tot;
+}
+
+
+function data_cashier()
+{
+  var db = new DataBase();
+  var myCashiers = db.getTable("cashier",'\\views\\cashier',2);
+  var ultimo = myCashiers.length-1;
+  monto = myCashiers[ultimo].money_open;
+  $("#monto").text(monto);
+  
+  var list_of_sales = returnlistofsalesofdate(new Date());
+  total_daySales = gettotalofsales(list_of_sales);
+  $("#totalSales").text(total_daySales);
+
+  total = parseInt(monto) + parseInt(total_daySales);
+  $("#total").text(total);
+  return total;
+}
+
+/*function apertura_cashier()
+{
+  var db = new DataBase();
+  var myCashiers = db.getTable("cashier",'\\views\\cashier',2);
+  var ultimo = myCashiers.length-1;
+  ultimo_cierre = myCashiers[ultimo].money_close;
+  $("#ultimocierre").text(ultimo_cierre);
+}*/
 
 function getpathproyect(todelete,cant_of_breakbar)
 {
