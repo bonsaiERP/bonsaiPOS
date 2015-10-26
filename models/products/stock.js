d672a6cdf0f67a2ff6dfb95bd14a1040f394e97a
PatriciaRosembluth <patty_rosembluth@hotmail.com>
2015-10-26 11:51:43 -0400

    Buscar producto por nombre y codigo en stock

--- models/products/stock.js
+++ models/products/stock.js
@@ -3,7 +3,8 @@
 var fs = require('fs');
 var db = new DataBase();
 var myObject = db.getTable("products",'\\views\\products',2);
-
+var products = [];
+getProducts();
 function getpathproyect(todelete,cant_of_breakbar)
 {
   actualdir = __dirname
@@ -45,6 +46,10 @@ function showAlertMessage(tipeMessage)
     $("#alertMessage").addClass("alert alert-dismissible alert-danger");
     $("#alertMessage")[0].innerHTML='<p>El producto no existe en el stock.</p>';
   }
+  if(tipeMessage=="warning"){
+    $("#alertMessage").addClass("alert alert-dismissible alert-danger");
+    $("#alertMessage")[0].innerHTML='<p>Debe ingresar el nombre o codigo del producto.</p>';
+  }
   $("#alertMessage").show();
 }
 
@@ -53,13 +58,26 @@ function showAlertMessage(tipeMessage)
     $("#alertMessage").hide();
     event.preventDefault();
     var data_table = $("#tblDatos");
+    var code_product = $("#code_product").val();
+    var resp = false;
+
+    //Vaciar la tabla
     if(data_table.length > 0){
       data_table.empty();
       data_table.append('<tr ><th style="text-align: center;">Id</th><th style="text-align: center;">C&oacute;digo</th><th style="text-align: center;">Nombre del producto</th><th style="text-align: center;">Precio Unitario</th><th style="text-align: center;">Cantidad</th></tr>');
     }
-    var code_product = $("#code_product").val();
-    var resp = false;
-    var refresh = false;
+    //Verificar si el text box no esta vacio
+    if (code_product != "") {
+      //Verificar si el codigo del producto no nombre del producto
+      if(Number(code_product) != true){
+        for (var cont = 0; cont < myObject.length; cont++) {
+          if (code_product === myObject[cont].name) {
+             code_product = myObject[cont].id;
+          }
+        }
+
+      }
+      //realizar la busqueda del producto en stock
       for (var cont = 0; cont < myObject.length; cont++) {
         if (code_product == myObject[cont].id) {
           data_table.append("<tr id = " + myObject[cont].id + '><td style="text-align: center;" ' + ">" + myObject[cont].id + "</td><td>" + myObject[cont].code + '</td><td style="text-align: center;">' + myObject[cont].name + '</td><td style="text-align: center;">' + myObject[cont].price  + '</td><td style="text-align: center;">' + myObject[cont].amount + "</td></tr>");
@@ -68,15 +86,29 @@ function showAlertMessage(tipeMessage)
           break;
         }
       }
-    if (resp === false) {
-      showAlertMessage("danger");
-      $("#code_product").val("");
+      //en caso de no encontrar el producto mostrar mensaje
+      if (resp === false) {
+        showAlertMessage("danger");
+        $("#code_product").val("");
+      }
+    }
+    else {
+      showAlertMessage("warning");
     }
   });
 
-$("#add_btn").click(function () {
-  showAlertMessage("danger");
-});
-
-
 })(jQuery);
+
+function getProducts() {
+  i = 0;
+  for (var cont = 0; cont < myObject.length; cont++) {
+    products[i] = myObject[cont].name;
+    i++;
+  }
+}
+
+$(document).ready(function() {
+		$('#code_product').autocomplete({
+			source: products
+		});
+	});
