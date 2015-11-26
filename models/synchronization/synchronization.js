3e876f49ca6a1873705298a1d7af98f58767f95a
NavVar <erika_vn@outlook.com>
2015-11-26 07:42:52 -0800

    se agrego la hora para sincronizacion diaria

--- models/synchronization/synchronization.js
+++ models/synchronization/synchronization.js
@@ -5,14 +5,22 @@ $(document).ready(function(){
   	var type='';
     var mySynchronization = [];
 
+$(document).ready(function () {
+        var ckbox = $('#daily');
+          $('#daily').on('click', function() {
+            if (ckbox.is(':checked')) {
+                alert('Debe seleccionar una hora o Se guardara con hora por defecto 9:00pm');
+            }
+        });
+});
 
 
       $("#checkbox_check").click(function() {
 
         if($("#daily").is(':checked') &&  $("#change").is(':checked')) {
 
-
-          var synchronizationDaily = { "id": 1, "type": "daily" };
+          var valor = document.getElementById("time").value;
+          var synchronizationDaily = { "id": 1, "type": "daily", "valor": valor };
           mySynchronization.push(synchronizationDaily);
           db.putTable("synchronization", mySynchronization,'\\views\\synchronization',2);
           var synchronizationDaily = { "id": 2, "type": "change" };
@@ -26,12 +34,13 @@ $(document).ready(function(){
 
           if($("#daily").is(':checked')) {
 
-
-            var synchronizationDaily = { "id": 1, "type": "daily" };
+            var valor = document.getElementById("time").value;
+            var synchronizationDaily = { "id": 1, "type": "daily", "valor": valor };
             mySynchronization.push(synchronizationDaily);
             db.putTable("synchronization", mySynchronization,'\\views\\synchronization',2);
             location.reload();
             localStorage.setItem('reload',1);
+
 
               alert("Activado: Sincronizacion diaria");
           }
