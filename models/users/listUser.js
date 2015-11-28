7fe3eb2553a35a485961220b224ced9eaa7785f9
OlmeClau <milenkaclaure18@gmail.com>
2015-11-28 18:47:26 -0400

    parte del buscar cliente funcionando

--- models/users/listUser.js
+++ models/users/listUser.js
@@ -42,11 +42,33 @@ function getClients() {
 
      $('#search_client').click(function () {
        //diercto para buscar
-       for (var cont = 0; cont < general_list_of_users.length; cont++) {
-         clients[i] = general_list_of_users[cont].name;
-         i++;
-       }
-       showAlertMessage("warning");
+       var name_search= $("#client_name").val();
+       var existe=false;
+       $("#alertMessage").hide();
+
+       var list_of_users = general_list_of_users;
+
+       var data_table = $("#datatable");
+       data_table.empty();
+       data_table.append('<tr ><th style="text-align: center;">Nombre</th><th style="text-align: center;">Apellido</th><th style="text-align: center;">CI</th><th style="text-align: center;">Opciones</th></tr>');
+
+       for (var i = 0; i < list_of_users.length; i++) {
+         if (list_of_users[i].name.toString()==name_search.toString()) {
+           existe=true;
+           data_table.append('<input type="hidden" value="'+ list_of_users[i].id +'" id="hidden_value"');
+            data_table.append('<tr> <td style="text-align: center;">' + list_of_users[i].name + '</td> <td style="text-align: center;">' + list_of_users[i].lastname + '</td> <td style="text-align: center;">' + list_of_users[i].ci + '<td style="text-align: center;"> <button onclick="to_editUser('+list_of_users[i].id+')">Editar usuario</button> </td></tr>');
+
+
+         }
+
+
+        }
+         if (!existe) {
+           showAlertMessage("warning");
+
+         }
+
+
      });
      $("#client_name").autocomplete(clients);//no funciona
 
