513f8dfd3380a29610fa473a913d0dc3fc63259a
Jorge de Ugarte <jdeugarte@githumb.com>
2015-11-30 18:31:29 -0400

    Anadir boton de crear cliente y direccionamiento de creacion de cliente modificado

--- models/users/newUser.js
+++ models/users/newUser.js
@@ -52,7 +52,67 @@ $(document).ready(function(){
       var user = { "id": id, "name": name, "lastname": lastname, "ci" : ci, "date": date, "sync": false };
       myUser.push(user);
       db.putTable("users", myUser,'\\views\\users',2);
-      location.reload();
+      location.replace('../../views/users/listusers.html');
+      localStorage.setItem('reload',1);
+      $('#alertMessage').html('Cliente creado con &eacute;xito.');
+      $('#alertMessage').show;
+  };
+});
+$(document).ready(function(){
+	var fs = require('fs');
+	var db = new DataBase();
+	var name='';
+	var lastname='';
+	var ci = 0;
+	var myUser = db.getTable("users",'\\views\\users',2);
+
+
+	$('#btn-new-user').click(function(){
+		event.preventDefault();
+		var error= false;
+		$('#modalBodyMessageDanger').html('');
+
+		if (!$('#name-field').val()) {
+			$('#modalBodyMessageDanger').append('<p>El campo <b>nombre</b> deben estar lleno.</p>');
+			error=true;
+		}
+
+		if (!$('#lastname-field').val()) {
+			$('#modalBodyMessageDanger').append('<p>El campo <b>apellido</b> deben estar lleno</p>');
+			error=true;
+		}
+
+		if (!$('#ci-field').val()) {
+			$('#modalBodyMessageDanger').append('<p>El campo <b>CI</b> deben estar lleno</p>');
+			error=true;
+		}
+
+		if (!$.isNumeric($('#ci-field').val())) {
+			$('#modalBodyMessageDanger').append('<p>El <b>CI</b> debe ser un valor numerico.</p>');
+			error=true;
+		}
+		if (error) {
+			$('#myDangerModal').modal('show');
+		} else{
+			name=$('#name-field').val();
+			lastname=$('#lastname-field').val();
+			ci=$('#ci-field').val();
+			saveUser();
+		}
+	});
+
+	function saveUser() {
+      var date = new Date().toUTCString();
+      var size = myUser.length;
+			var id = 1;
+      if(myUser.length != 0){
+        var aux = myUser.length;
+        id = myUser[aux-1].id + 1;
+      }
+      var user = { "id": id, "name": name, "lastname": lastname, "ci" : ci, "date": date, "sync": false };
+      myUser.push(user);
+      db.putTable("users", myUser,'\\views\\users',2);
+      location.replace('../../views/users/listusers.html');
       localStorage.setItem('reload',1);
       $('#alertMessage').html('Cliente creado con &eacute;xito.');
       $('#alertMessage').show;
